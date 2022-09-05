464bf8bc35fc1bd912f8a9dd503c69f494d96a0f
mehdi_mohammadi <mehdimuhammadi3@gmail.com>
2022-09-05 18:59:30 +0430

    finish Slider  One

--- js/script.js
+++ js/script.js
@@ -9,13 +9,46 @@ const swiper1 = new Swiper(".swiper1", {
 		prevEl: ".newst_prev",
 	},
 	breakpoints: {
-		300: { slidesPerView: 1, spaceBetween: 5},
-		700: { slidesPerView: 2, spaceBetween: 5},
-		1300: { slidesPerView: 3, spaceBetween: 25},
+		300: { slidesPerView: 1, spaceBetween: 5 },
+		700: { slidesPerView: 2, spaceBetween: 5 },
+		1300: { slidesPerView: 3, spaceBetween: 25 },
 	},
 });
 
-const swiper1Img = new Swiper(".swiper1Img", {
+const swiper1Img1 = new Swiper(".swiper1Img1", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+	navigation: {
+		nextEl: ".next_img",
+		prevEl: ".prev_img",
+	},
+});
+const swiper1Img2 = new Swiper(".swiper1Img2", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+	navigation: {
+		nextEl: ".next_img",
+		prevEl: ".prev_img",
+	},
+});
+const swiper1Img3 = new Swiper(".swiper1Img3", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+	navigation: {
+		nextEl: ".next_img",
+		prevEl: ".prev_img",
+	},
+});
+const swiper1Img4 = new Swiper(".swiper1Img4", {
 	// Optional parameters
 	direction: "horizontal",
 	slidesPerView: 1,
@@ -27,17 +60,17 @@ const swiper1Img = new Swiper(".swiper1Img", {
 	},
 });
 
-const swiper2Img = new Swiper(".swiper2Img", {
-	// Optional parameters
-	direction: "horizontal",
-	slidesPerView: 1,
-	spaceBetween: 0,
-	loop: true,
-	navigation: {
-		nextEl: ".next_img",
-		prevEl: ".prev_img",
-	},
-});
+// const swiper2Img = new Swiper(".swiper2Img", {
+// 	// Optional parameters
+// 	direction: "horizontal",
+// 	slidesPerView: 1,
+// 	spaceBetween: 0,
+// 	loop: true,
+// 	navigation: {
+// 		nextEl: ".next_img",
+// 		prevEl: ".prev_img",
+// 	},
+// });
 
 const swiper2 = new Swiper(".swiper2", {
 	// Optional parameters
@@ -53,7 +86,7 @@ const swiper2 = new Swiper(".swiper2", {
 		// 300: { slidesPerView: 1, spaceBetween: 15},
 		// 768: { spaceBetween: 2, spaceBetween: 15},
 		// 1200: { spaceBetween: 5 },
-		300: { slidesPerView: 1, spaceBetween: 5},
+		300: { slidesPerView: 1, spaceBetween: 5 },
 		768: { slidesPerView: 2 },
 	},
 });
@@ -69,9 +102,9 @@ const swiper3 = new Swiper(".swiper3", {
 		prevEl: ".weblog_prev",
 	},
 	breakpoints: {
-		300: { slidesPerView: 2,spaceBetween: 3, },
-		768: { spaceBetween: 3, slidesPerView: 1, },
-		1200: { spaceBetween: 3, slidesPerView: 1, },
+		300: { slidesPerView: 2, spaceBetween: 3 },
+		768: { spaceBetween: 3, slidesPerView: 1 },
+		1200: { spaceBetween: 3, slidesPerView: 1 },
 	},
 });
 
@@ -93,8 +126,8 @@ const swiper4 = new Swiper(".swiper4", {
 	},
 
 	pagination: {
-		el: '.swiper-pagination',
-	  },
+		el: ".swiper-pagination",
+	},
 });
 
 const swiper5 = new Swiper(".swiper5", {
@@ -113,40 +146,37 @@ const swiper6 = new Swiper(".swiper6", {
 	loop: true,
 });
 
+const searchSelectBtn = document.querySelectorAll(".search_select");
 
-
-const searchSelectBtn = document.querySelectorAll('.search_select')
-
-searchSelectBtn.forEach(list => {
-	list.addEventListener('click', e => {
+searchSelectBtn.forEach((list) => {
+	list.addEventListener("click", (e) => {
 		e.preventDefault();
 
-		const ulElem = list.querySelector('ul');
-		const svgElem = list.querySelector('svg');
+		const ulElem = list.querySelector("ul");
+		const svgElem = list.querySelector("svg");
 
-		if (ulElem.style.height == '0px') {
-			ulElem.style.height = '10rem'
-			ulElem.style.padding = '1rem 2rem'
-			svgElem.style.transform = 'rotate(0)'
-		} else  {
-			ulElem.style.height = '0px'
-			ulElem.style.padding = '0'
-			svgElem.style.transform = 'rotate(-90deg)'
+		if (ulElem.style.height == "0px") {
+			ulElem.style.height = "10rem";
+			ulElem.style.padding = "1rem 2rem";
+			svgElem.style.transform = "rotate(0)";
+		} else {
+			ulElem.style.height = "0px";
+			ulElem.style.padding = "0";
+			svgElem.style.transform = "rotate(-90deg)";
 		}
-	})
-})
+	});
+});
 
-const hamburgerMenu = document.querySelector('#hamburger_menu')
+const hamburgerMenu = document.querySelector("#hamburger_menu");
 
-hamburgerMenu.addEventListener ('click', () => {
-	document.querySelector('.mobile_menu').style.width = '100%'
-	document.querySelector('.mobile_menu').style.padding = '1.9rem 2.8rem'
-})
+hamburgerMenu.addEventListener("click", () => {
+	document.querySelector(".mobile_menu").style.width = "100%";
+	document.querySelector(".mobile_menu").style.padding = "1.9rem 2.8rem";
+});
 
-const mobileExitBtn = document.querySelector('.exitBtn')
+const mobileExitBtn = document.querySelector(".exitBtn");
 
-mobileExitBtn.addEventListener('click', () => {
-	document.querySelector('.mobile_menu').style.width = '0'
-	document.querySelector('.mobile_menu').style.padding = '0'
-
-})
+mobileExitBtn.addEventListener("click", () => {
+	document.querySelector(".mobile_menu").style.width = "0";
+	document.querySelector(".mobile_menu").style.padding = "0";
+});
