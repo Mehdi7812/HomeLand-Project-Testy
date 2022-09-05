bc58616329a2144753cdd3f49b6241e9b29eb12c
mehdi_mohammadi <mehdimuhammadi3@gmail.com>
2022-09-05 19:04:53 +0430

    final Design

---
+++ js/script.js
@@ -0,0 +1,151 @@
+const swiper1 = new Swiper(".swiper1", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 4,
+	spaceBetween: 25,
+	loop: true,
+	navigation: {
+		nextEl: ".newst_next",
+		prevEl: ".newst_prev",
+	},
+	breakpoints: {
+		300: { slidesPerView: 1, spaceBetween: 5},
+		768: { slidesPerView: 2 },
+	},
+});
+
+const swiper1Img = new Swiper(".swiper1Img", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+	navigation: {
+		nextEl: ".next_img",
+		prevEl: ".prev_img",
+	},
+});
+
+const swiper2Img = new Swiper(".swiper2Img", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+	navigation: {
+		nextEl: ".next_img",
+		prevEl: ".prev_img",
+	},
+});
+
+const swiper2 = new Swiper(".swiper2", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 25,
+	loop: true,
+	navigation: {
+		nextEl: ".proposal_next",
+		prevEl: ".proposal_prev",
+	},
+	breakpoints: {
+		// 300: { slidesPerView: 1, spaceBetween: 15},
+		// 768: { spaceBetween: 2, spaceBetween: 15},
+		// 1200: { spaceBetween: 5 },
+		300: { slidesPerView: 1, spaceBetween: 5},
+		768: { slidesPerView: 2 },
+	},
+});
+
+const swiper3 = new Swiper(".swiper3", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 3,
+	spaceBetween: 25,
+	loop: true,
+	navigation: {
+		nextEl: ".weblog_next",
+		prevEl: ".weblog_prev",
+	},
+	breakpoints: {
+		300: { slidesPerView: 2,spaceBetween: 3, },
+		768: { spaceBetween: 3, slidesPerView: 1, },
+		1200: { spaceBetween: 3, slidesPerView: 1, },
+	},
+});
+
+const swiper4 = new Swiper(".swiper4", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 5,
+	spaceBetween: 5,
+	loop: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	breakpoints: {
+		300: { slidesPerView: 3 },
+		1200: { slidesPerView: 4 },
+		1400: { slidesPerView: 5 },
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+	  },
+});
+
+const swiper5 = new Swiper(".swiper5", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 10,
+	loop: true,
+});
+
+const swiper6 = new Swiper(".swiper6", {
+	// Optional parameters
+	direction: "horizontal",
+	slidesPerView: 1,
+	spaceBetween: 5,
+	loop: true,
+});
+
+
+
+const searchSelectBtn = document.querySelectorAll('.search_select')
+
+searchSelectBtn.forEach(list => {
+	list.addEventListener('click', e => {
+		e.preventDefault();
+
+		const ulElem = list.querySelector('ul');
+		const svgElem = list.querySelector('svg');
+
+		if (ulElem.style.height == '0px') {
+			ulElem.style.height = '10rem'
+			ulElem.style.padding = '1rem 2rem'
+			svgElem.style.transform = 'rotate(0)'
+		} else  {
+			ulElem.style.height = '0px'
+			ulElem.style.padding = '0'
+			svgElem.style.transform = 'rotate(-90deg)'
+		}
+	})
+})
+
+const hamburgerMenu = document.querySelector('#hamburger_menu')
+
+hamburgerMenu.addEventListener ('click', () => {
+	document.querySelector('.mobile_menu').style.width = '100%'
+	document.querySelector('.mobile_menu').style.padding = '1.9rem 2.8rem'
+})
+
+const mobileExitBtn = document.querySelector('.exitBtn')
+
+mobileExitBtn.addEventListener('click', () => {
+	document.querySelector('.mobile_menu').style.width = '0'
+	document.querySelector('.mobile_menu').style.padding = '0'
+
+})
